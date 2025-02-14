c859e9ccf7376c855141dbf181d551a13be94800
Harsh Jindal <jindalharsh457@gmail.com>
2025-02-14 16:24:41 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = False
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI with Docker!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"item_name": item.name, "item_price": item.price}
